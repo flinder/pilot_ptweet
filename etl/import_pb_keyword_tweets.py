bd5e0784ff0d1057a174c89fe0df2afca323efaa
Fridolin Linder <fridolin.linder@gmail.com>
2016-10-21 11:05:59 -0400

    tidied up import code

--- etl/import_pb_keyword_tweets.py
+++ etl/import_pb_keyword_tweets.py
@@ -7,6 +7,7 @@ import time
 import gzip
 import sqlalchemy
 import os
+from sqlalchemy.sql import exists
 
 # Custom Imports 
 sys.path.append('../database/')
@@ -27,131 +28,142 @@ def is_valid(tweet):
             tweet[k]
         except KeyError:
             return False
-    if not keyword_in(tweet):
-        return False
     return True
 
-# Parameters 
-ARCHIVES = ['../../data/pablos_stuff/keyword_tweets/germany-2013-03.tar',
-            '../../data/pablos_stuff/keyword_tweets/germany-2013-04.tar',
-            '../../data/pablos_stuff/keyword_tweets/germany-2013-05.tar',
-            '../../data/pablos_stuff/keyword_tweets/germany-2016-06.tar',
-            '../../data/pablos_stuff/keyword_tweets/germany-2016-07.tar',
-            '../../data/pablos_stuff/keyword_tweets/germany-2016-08.tar',
-            '../../data/pablos_stuff/keyword_tweets/germany-2016-09.tar',
-            ]
-
-# Set up sqlalchemy session
-session = make_session()
-
-for archive in ARCHIVES:
+def read_tweet(line):
+    '''
+    Read line from infile, check if valid json, check if valid tweet (has all
+    required fields) and check if relevant tweet (has relevant keywords)
+    '''
     
-    print('Processing {}...'.format(os.path.basename(archive)))
-    with tarfile.open(archive, 'r') as archive:
+    # Read in line, if decoder can't recognize json exit
+    try:
+        tweet = json.loads(line)
+    except json.decoder.JSONDecodeError:
+        return None
 
-        decode_err = 0
-        tweet_count = 0
-        for i, entry in enumerate(archive):
+    # Check if all necessary fields are present
+    if not is_valid(tweet):
+        return None
+
+    # Check if tweet has any of the keywords 
+    if not keyword_in(tweet):
+        return None
+
+    # If all checks pass return the tweet as dictionary
+    return tweet
+
+def make_sql(tweet):
+    '''
+    Transforms json tweet into sqlalchemy User and Tweet objects as defined
+    in /database/db.py
+    '''
+    
+    tt = tweet.get('created_at', None)
+    if tt is not None:
+        tt = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(
+            tt, '%a %b %d %H:%M:%S +0000 %Y'))
+ 
+    user = tweet['user']
+    uid = int(user['id_str'])
+    ca = user.get('created_at', None)
+    if ca is not None:
+        ca = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(
+            ca,'%a %b %d %H:%M:%S +0000 %Y'))
+
+    u = User(id=uid,
+             contributors=user.get('contributors_enabled', None),
+             created_at=ca,
+             description=user.get('description', None),
+             followers_count=user.get('followers_count', None),
+             friends_count=user.get('friends_count', None),
+             geo_enabled=user.get('geo_enabled', None),
+             lang=user.get('lang', None),
+             listed_count=user.get('listed_count', None),
+             location=user.get('location', None),
+             name=user.get('name', None),
+             screen_name=user.get('screen_name', None),
+             statuses_count=user.get('statuses_count', None),
+             time_zone=user.get('time_zone', None),
+             verified=user.get('verified', None),
+             data_group='pb_keyword',
+             profile_date=tt # From tweet
+             )
+
+    # Handle all tweet information
+    id_ = int(tweet['id_str'])
+    
+    ## Handle Optional tweet fields
+
+    if tweet['coordinates'] is None:
+        lon = None
+        lat = None
+    else:
+        lon=tweet['coordinates']['coordinates'][0],
+        lat=tweet['coordinates']['coordinates'][1],
+
+    ## Make mapper object
+    t = Tweet(id=id_,
+              is_quote_status=tweet.get('is_quote_status', None),
+              in_reply_to_status_id=tweet.get('in_reply_to_status_id', None),
+              in_reply_to_user_id=tweet.get('in_reply_to_user_id', None),
+              quoted_status_id=tweet.get('quoted_status_id', None),
+              source=tweet.get('source', None),
+              longitude=lon,
+              latitude=lat,
+              lang=tweet.get('lang', None),
+              created_at=tt,
+              retweet_count=tweet.get('retweet_count', None),
+              favorite_count=tweet.get('favorite_count', None),
+              text=tweet.get('text', None), 
+              user_id=uid,
+              data_group='pb_keyword')
+
+    return u, t, uid, id_
+
+
+if __name__ == "__main__":
+
+
+    # Parameters 
+    INFILE = '../../data/pablos_stuff/keyword_tweets/pb_keyword_tweets.json'
+
+    # Set up sqlalchemy session
+    session = make_session()
+        
+    with io.open(INFILE, 'r') as infile:
             
-            # Skip folders
-            if entry.isdir():
+        invalid_lines = 0
+        to_commit = []
+        ts = 0
+        us = 0
+        for ln, line in enumerate(infile):
+
+            tweet = read_tweet(line)
+            if tweet is None:
+                invalid_lines += 1
                 continue
 
-            # Get file out of tar archive
-            extracted = archive.extractfile(entry)
+            u, t, uid, tid = make_sql(tweet) # user, tweet, userid, tweetid
 
-            # Decompress
-            with gzip.open(extracted, 'rb') as infile:
+            ## Insert them to database
+            user_exists = session.query(exists().where(User.id==uid)).scalar()
+            tweet_exists = session.query(exists().where(Tweet.id==tid)).scalar()
+            
+            if not user_exists:
+                session.add(u)
+                us += 1
+            if not tweet_exists:
+                session.add(t)
+                ts += 1
+             
+
+            if ln % 10000 == 0:
+                print("Processed {} lines. {} relevant tweets, "
+                      "{} tweets inserted, {} users inserted."
+                      "Committing to db...".format(ln, ln - invalid_lines, ts, 
+                                                   us))
+                #session.add_all(to_commit)
+                session.commit()
                 
-                # Process lines (each line a tweet)
-                for ln, line in enumerate(infile):
-                    try:
-                        tweet = json.loads(line.decode("utf-8"))
-                        tweet_count += 1
-                    except json.decoder.JSONDecodeError as e:
-                        print(ln, e)
-                        decode_err += 1
-                        continue
-                    
-                    # Check if tweet is relevant and valid
-                    if not is_valid(tweet):
-                        continue
 
-                    # Handle all tweet information
-                    id_ = int(tweet['id_str'])
-                    
-                    ## Handle Optional tweet fields
-                    try:
-                        qsid = tweet['quoted_status_id']
-                    except KeyError:
-                        qsid = None
-
-                    if tweet['coordinates'] is None:
-                        lon = None
-                        lat = None
-                    else:
-                        lon=tweet['coordinates']['coordinates'][0],
-                        lat=tweet['coordinates']['coordinates'][1],
-                   
-                    ts = time.strftime('%Y-%m-%d %H:%M:%S', 
-                                       time.strptime(tweet['created_at'],
-                                                     '%a %b %d %H:%M:%S +0000 %Y'))
-                    ## Make mapper object
-                    t = Tweet(id=int(tweet['id_str']),
-                              is_quote_status=tweet['is_quote_status'],
-                              in_reply_to_status_id=tweet['in_reply_to_status_id'],
-                              in_reply_to_user_id=tweet['in_reply_to_user_id'],
-                              quoted_status_id=qsid,
-                              source=tweet['source'],
-                              longitude=lon,
-                              latitude=lat,
-                              lang=tweet['lang'],
-                              created_at=ts,
-                              retweet_count=tweet['retweet_count'],
-                              favorite_count=tweet['favorite_count'],
-                              text=tweet['text'], 
-                              user_id=tweet['user']['id'],
-                              data_group='pb_keyword')
-                    ## Add to db session 
-                    try:
-                        session.add(t)
-                        session.commit()
-                    except sqlalchemy.exc.IntegrityError as e:
-                        session.rollback()
-
-
-
-                    # Handle user information
-
-                    ## Check if user is already in db
-                    user = tweet['user']
-                    uid = int(user['id_str'])
-                    
-                    uts = time.strftime('%Y-%m-%d %H:%M:%S', 
-                                       time.strptime(user['created_at'],
-                                                     '%a %b %d %H:%M:%S +0000 %Y'))
-                    u = User(id=uid,
-                             contributors=user['contributors_enabled'],
-                             created_at=uts,
-                             description=user['description'],
-                             followers_count=user['followers_count'],
-                             friends_count=user['friends_count'],
-                             geo_enabled=user['geo_enabled'],
-                             lang=user['lang'],
-                             listed_count=user['listed_count'],
-                             location=user['location'],
-                             name=user['name'],
-                             screen_name=user['screen_name'],
-                             statuses_count=user['statuses_count'],
-                             time_zone=user['time_zone'],
-                             verified=user['verified'],
-                             data_group='pb_keyword',
-                             profile_date=ts # From tweet
-                             )
-                    try:
-                        session.add(u)
-                        session.commit()
-                    except sqlalchemy.exc.IntegrityError as e:
-                        session.rollback()
-
-        print("Processed {} tweets.".format(tweet_count))
